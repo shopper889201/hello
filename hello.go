7479cc9ce29fac4068b267d61940f243e0710dd5
shopper889201 <shopper889201@outlook.com>
2015-11-23 23:03:51 -0600

    with reversed string code in

--- hello.go
+++ hello.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/shopper889201/stringutil"
+)
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!og, olleH"))
 }
 
